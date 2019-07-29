dcb707f35cafd2e4bda0ee94a82c09547b70b446
joenerv <joenerv@yandex.ru>
2019-07-29 13:53:45 +0300

    j

--- novoc layout/gulp/tasks/watch.js
+++
@@ -1,7 +0,0 @@
-module.exports = function () {
-    $.gulp.task('watch',function () {
-        $.gulp.watch('app/pug/*.pug', $.gulp.series('pug'));
-        $.gulp.watch('app/sass/*.sass', $.gulp.series('sass'));
-    });
-    
-}; 
