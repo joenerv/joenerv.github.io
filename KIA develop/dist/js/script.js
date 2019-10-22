4612162f39ec8237127da21dae89f1179408e6f5
joenerv <joenerv@yandex.ru>
2019-10-22 16:22:19 +0300

    ver 2.0

--- KIA develop/dist/js/script.js
+++ KIA develop/dist/js/script.js
@@ -1,45 +1,7 @@
-window.addEventListener('DOMContentLoaded', function() {
-
-    'use strict';
-
-    let arrowOne   = document.querySelector('.slider-text-block1__arrow1'),
-        arrowTwo   = document.querySelector('.slider-text-block2__arrow2'),
-        arrowThree = document.querySelector('.slider-text-block3__arrow3'),
-        arrowFour  = document.querySelector('.slider-text-block4__arrow4'),
-        textOne    = document.querySelector('.slider-text-block1__text1'),
-        textTwo    = document.querySelector('.slider-text-block2__text2'),
-        textThree  = document.querySelector('.slider-text-block3__text3'),
-        textFour   = document.querySelector('.slider-text-block4__text4'),
-        block      = document.querySelector('.slider-text'),
-        arrowAll   = block.getElementsByTagName('img');
-
-
-    textOne.addEventListener('click', function() {
-        
-        for(let i=0; i < arrowAll.length; i = i + 1) {
-            arrowAll[i].style.display = "none";
-        }
-        arrowOne.style.display = 'block';
+$(document).ready(function(){
+    
+    $('.popup__close').click(function(){
+        $('.popup').css('display', 'none');
     });
 
-    textTwo.addEventListener('click', function() {
-        for(let i=0; i < arrowAll.length; i = i + 1) {
-            arrowAll[i].style.display = "none";
-        }
-        arrowTwo.style.display = 'block';
-    });
-
-    textThree.addEventListener('click', function() {
-        for(let i=0; i < arrowAll.length; i = i + 1) {
-            arrowAll[i].style.display = "none";
-        }
-        arrowThree.style.display = 'block';
-    });
-
-    textFour.addEventListener('click', function() {
-        for(let i=0; i < arrowAll.length; i = i + 1) {
-            arrowAll[i].style.display = "none";
-        }
-        arrowFour.style.display = 'block';
-    });
 });
