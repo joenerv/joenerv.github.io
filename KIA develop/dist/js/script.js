9955cba9977f76d2d82323fa6e16b33e684c1e9e
joenerv <joenerv@yandex.ru>
2019-10-23 14:15:18 +0300

    ver 3.0

--- KIA develop/dist/js/script.js
+++ KIA develop/dist/js/script.js
@@ -4,4 +4,16 @@ $(document).ready(function(){
         $('.popup').css('display', 'none');
     });
 
+    $('.menu-close').click(function(){
+        $('.menu').css('left', '-505px');
+    });
+    
+    $('.header__menu').click(function(){
+        $('.menu').css('left', '0');
+    });
+
+    $('.menu-auto').click(function(){
+        $('.menu').css('left', '-505px');
+    });
+
 });
