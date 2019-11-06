0a89ef149d4f4349a6a8c58742dc551c51f21c6a
joenerv <joenerv@yandex.ru>
2019-11-07 00:15:45 +0300

    ver 3.1

--- 9line/ninelines-template-master/src/js/main.js
+++ 9line/ninelines-template-master/src/js/main.js
@@ -1,1 +1,11 @@
 import './vendor';
+$(document).ready(function(){ 
+    $("#name").on("keypress", function(e) {
+  
+        var char = /[A-Za-zА-Яа-яЁё]/;
+        var val = String.fromCharCode(e.which);
+        var test = char.test(val);
+        
+        if(!test) return false
+      })
+});
