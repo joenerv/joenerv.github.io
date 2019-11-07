8aae228ff3f7b8d28917d1116603171fcac77ee9
joenerv <joenerv@yandex.ru>
2019-11-07 16:21:19 +0300

    вв

--- 9line/ninelines-template-master/src/js/main.js
+++ 9line/ninelines-template-master/src/js/main.js
@@ -2,9 +2,9 @@ import './vendor';
 $(document).ready(function(){ 
     $("#name").on("keypress", function(e) {
   
-        var char = /[A-Za-zА-Яа-яЁё]/;
-        var val = String.fromCharCode(e.which);
-        var test = char.test(val);
+        let char = /[A-Za-zА-Яа-яЁё]/;
+        let val = String.fromCharCode(e.which);
+        let test = char.test(val);
         
         if(!test) return false
       })
