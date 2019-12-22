a4d4e719df555699e6ef2e5dd5ad02f924cb4402
joenerv <joenerv@yandex.ru>
2019-12-23 01:32:17 +0300

    v/1.0

--- курс JS веб разраб/java.js
+++ курс JS веб разраб/java.js
@@ -1,87 +1,23 @@
 'use strict';
 
-let money, 
-	name, 
-	time,
-	price
+var budget = +prompt('Ваш бюджет', '15000');
+var nameMagazine = prompt('Введите неазвание магазина', '5-ka');
 
-function start() {
-	money = prompt('Ваш бюджет?', '');
-	
-	while (isNaN(money) || money == '' || money == null) {
-		money = prompt('Ваш бюджет?', '');
-	}
-	name = prompt('Название магазина', '').toUpperCase();
-	time = 21;
-}
-
-// start();
-
-let mainList = {
-	budget: money,
-	shopName: name,
-	shopGoods: [],
+var mainList = {
+	budgetMany: budget,
+	name: nameMagazine,
+	shopGoods: ['apple', 'tel', 'water'],
 	employers: {},
-	open: false,
-	discount: true
-};
-
-function chooseGoods() {
-	for (let i = 0; i < 5; i++) {
-		let a = prompt('Какой тип товара будем продавать?');
-		
-		if ((typeof(a)) === 'string' && (typeof(a)) != null && a != '' && a.length < 50) {
-			console.log('все верно');
-			mainList.shopGoods[i] = a;
-		} else {
-			i = i - 1;
-		}
-	}
-}
-// chooseGoods();
-
-function workTime(time) {
-	if (time < 0) {
-		console.log('Такого не может быть');
-
-		} else if (time > 8 && time < 20) {
-			console.log('время работать')
-			}  else if(time < 24) {
-				console.log('Уже слишком поздно');
-			} else {
-				console.log('В сутках только 24 часа')
-			}; 
-};
-
-function budget() {
-	if (mainList.discount == true) {
-		alert((mainList.budget / 30)*0.8);
-	} else {
-		alert(mainList.budget / 30);
-	}
-}
-// budget();
-
-// console.log(mainList);
-
-
-let options = {
-	name: 'test',
-	width: 1024,
-	height: 1024
-}
-options.bool = 'false';
-options.colors = {
-	border: 'black',
-	background: 'red'
+	open: true
 }
 
-delete options.bool
+console.log(mainList);
 
-for( let key in options) {
-	console.log('svoistvo ' + key + ' imeet zna4enie' + options[key])
-}
 
-console.log(Object.keys(options).length);
+var type1 = prompt('какой тип товаров будем продавать?', 'картошка');
+var type2 = prompt('какой тип товаров будем продавать?', 'картошка');
+var type3 = prompt('какой тип товаров будем продавать?', 'картошка');
 
-console.log(options);
+var budgetDay = budget/30;
+
+alert('Ваш бюджет на 1 день ' + budgetDay + ' р.');
