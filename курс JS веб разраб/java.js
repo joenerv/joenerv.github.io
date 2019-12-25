b15082e7dc5f49f31c61440466b8dea8f6afdaa8
joenerv <joenerv@yandex.ru>
2019-12-25 21:39:54 +0300

    Update java.js

--- курс JS веб разраб/java.js
+++ курс JS веб разраб/java.js
@@ -1,23 +1,23 @@
-'use strict';
+// 'use strict';
 
-var budget = +prompt('Ваш бюджет', '15000');
-var nameMagazine = prompt('Введите неазвание магазина', '5-ka');
+// var budget = +prompt('Ваш бюджет', '15000');
+// var nameMagazine = prompt('Введите неазвание магазина', '5-ka');
 
-var mainList = {
-	budgetMany: budget,
-	name: nameMagazine,
-	shopGoods: ['apple', 'tel', 'water'],
-	employers: {},
-	open: true
-}
+// var mainList = {
+// 	budgetMany: budget,
+// 	name: nameMagazine,
+// 	shopGoods: ['apple', 'tel', 'water'],
+// 	employers: {},
+// 	open: true
+// }
 
-console.log(mainList);
+// console.log(mainList);
 
 
-var type1 = prompt('какой тип товаров будем продавать?', 'картошка');
-var type2 = prompt('какой тип товаров будем продавать?', 'картошка');
-var type3 = prompt('какой тип товаров будем продавать?', 'картошка');
+// var type1 = prompt('какой тип товаров будем продавать?', 'картошка');
+// var type2 = prompt('какой тип товаров будем продавать?', 'картошка');
+// var type3 = prompt('какой тип товаров будем продавать?', 'картошка');
 
-var budgetDay = budget/30;
+// var budgetDay = budget/30;
 
-alert('Ваш бюджет на 1 день ' + budgetDay + ' р.');
+// alert('Ваш бюджет на 1 день ' + budgetDay + ' р.');
