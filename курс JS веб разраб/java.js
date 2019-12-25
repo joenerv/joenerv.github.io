7a642684852d124b0e7eb5f6c3e145a727341adb
joenerv <joenerv@yandex.ru>
2019-12-25 16:33:38 +0300

    2

--- курс JS веб разраб/java.js
+++ курс JS веб разраб/java.js
@@ -1,23 +1,54 @@
 'use strict';
 
-var budget = +prompt('Ваш бюджет', '15000');
-var nameMagazine = prompt('Введите неазвание магазина', '5-ka');
+/*let money = +prompt('Ваш бюджет', '15000');
+let name = prompt('Введите неазвание магазина', '5-ka');
+let time = 19;
 
-var mainList = {
-	budgetMany: budget,
-	name: nameMagazine,
-	shopGoods: ['apple', 'tel', 'water'],
+let mainList = {
+	budget: money,
+	nameShop: name,
+	shopGoods: [],
 	employers: {},
 	open: true
 }
 
-console.log(mainList);
 
 
-var type1 = prompt('какой тип товаров будем продавать?', 'картошка');
-var type2 = prompt('какой тип товаров будем продавать?', 'картошка');
-var type3 = prompt('какой тип товаров будем продавать?', 'картошка');
+for (let i = 0; i < 5; i++) {
+	let a = prompt('какой тип товаров будем продавать?', 'картошка');
+	 
+	if ((typeof(a)) === 'string' && (typeof(a)) != null && a != '' && a.length < 50) {
+			console.log('все верно');
+			mainList.shopGoods[i] = a;
+		} else {
+			i = i - 1;
+		}
+}
 
-var budgetDay = budget/30;
+
+if (time < 0) {
+	console.log('такого не может быть');
+} else if(time > 8 && time < 20) {
+		console.log('магазин работает');		
+	} else if (time < 24) {
+		console.log('слишком поздно');
+		
+		} else {
+			console.log('такого не может быть');
+			}
+
+
+let budgetDay = money/30;
+
 
 alert('Ваш бюджет на 1 день ' + budgetDay + ' р.');
+
+console.log(mainList);*/
+
+let week = ['понед', 'вторник', 'среда', 'четверг', 'пятница', 'суббота', 'вскр'];
+
+let weekend = document.getElementById('mary');
+
+weekend.textContent = week;
+
+console.log(week);
