bdf8f3e26089f89db01e9da4c99a4e3f4e10f16e
joenerv <joenerv@yandex.ru>
2020-01-15 15:56:34 +0300

    up 1

---
+++ курс JS веб разраб/ПРОЕКТ ДЛЯ ДЗ (УРОК № 5)/js/script.js
@@ -0,0 +1,20 @@
+let open          = document.getElementById('open-btn'),
+    name          = document.querySelector('.name-value'),
+    budget        = document.querySelector('.budget-value'),
+    goods         = document.querySelector('.goods-value'),
+    items         = document.querySelector('.items-value'),
+    employers     = document.querySelector('.employers-value'),
+    discount      = document.querySelector('.discount-value'),
+    isopen        = document.querySelector('.isopen-value'),
+    goodsItem     = document.getElementsByClassName('goods-item'),
+    button        = document.getElementsByTagName('button'),
+    chooseItem    = document.querySelector('.choose-item'),
+    time          = document.querySelector('.time-value'),
+    countBudget   = document.querySelector('.count-budget-value'),
+    hireEmployers = document.querySelectorAll('.hire-employers-item');
+
+
+
+
+
+console.log(hireEmployers[1]);
