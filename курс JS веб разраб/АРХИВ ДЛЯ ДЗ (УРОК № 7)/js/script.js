b957331c05f8cf05020835a10a8706a3966c7f6f
joenerv <joenerv@yandex.ru>
2020-02-02 23:30:24 +0300

    upd

--- курс JS веб разраб/АРХИВ ДЛЯ ДЗ (УРОК № 7)/js/script.js
+++ курс JS веб разраб/АРХИВ ДЛЯ ДЗ (УРОК № 7)/js/script.js
@@ -81,7 +81,7 @@ window.addEventListener('DOMContentLoaded', function() {
 
     setClock('timer', deadline);
 
-    //mpodal
+    //modal
 
     let more = document.querySelector('.more'),
         overlay = document.querySelector('.overlay'),
@@ -100,6 +100,35 @@ window.addEventListener('DOMContentLoaded', function() {
         document.body.style.overflow = '';
     });
 
+    //animation
+
+    function animate(draw, duration) {
+        let start = performance.now();
+        requestAnimationFrame(
+            function animate(time) {
+                let timePassed = time - start;
+                if (timePassed > duration) {
+                    timePassed = duration;
+                }
+
+                draw(timePassed);
+
+                if (timePassed < duration) {
+                    requestAnimationFrame(animate);
+                }
+            })
+    };
+
+    let menu = document.getElementsByTagName('nav')[0];
+
+    menu.addEventListener('click', function(event) {
+        event.preventDefault();
+        animate(function(timePassed) {
+            let target = event.target;
+            let section = document.getElementById(target.getAttribute('href').slice(1));
+            window.scrollBy(0, section.getBoundingClientRect().top / 20 - 3);
+        }, 1500)
+    })
 
 
 });
