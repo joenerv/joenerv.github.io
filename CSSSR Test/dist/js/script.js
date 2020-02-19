76c503df739f06f53cd076ad58411d798051bae3
joenerv <joenerv@yandex.ru>
2020-02-19 16:07:33 +0300

    up

--- CSSSR Test/dist/js/script.js
+++ CSSSR Test/dist/js/script.js
@@ -2,44 +2,44 @@ window.addEventListener('DOMContentLoaded', function() {
 
     'use strict';
 
-    let arrowOne   = document.querySelector('.slider-text-block1__arrow1'),
-        arrowTwo   = document.querySelector('.slider-text-block2__arrow2'),
-        arrowThree = document.querySelector('.slider-text-block3__arrow3'),
-        arrowFour  = document.querySelector('.slider-text-block4__arrow4'),
-        textOne    = document.querySelector('.slider-text-block1__text1'),
+    let textOne    = document.querySelector('.slider-text-block1__text1'),
         textTwo    = document.querySelector('.slider-text-block2__text2'),
         textThree  = document.querySelector('.slider-text-block3__text3'),
         textFour   = document.querySelector('.slider-text-block4__text4'),
-        block      = document.querySelector('.slider-text'),
-        arrowAll   = block.getElementsByTagName('img');
-
-
-    textOne.addEventListener('click', function() {
+        slider = document.querySelector('.slider__range'),
+        textarea = document.querySelector('.story-form-enter');
         
-        for(let i=0; i < arrowAll.length; i = i + 1) {
-            arrowAll[i].style.display = "none";
-        }
-        arrowOne.style.display = 'block';
+    /*const initialHeight = 55;
+
+    const resize = () => {
+        textarea.style.height = `${initialHeight}px`;
+        const height = textarea.scrollHeight;
+        textarea.style.height = `${height + initialHeight}`;
+    };
+
+    resize();
+
+    textarea.addEventListener('input', resize);*/
+
+    textarea.addEventListener('scroll', function() {
+        
+    });
+
+    
+
+    textOne.addEventListener('click', function() {        
+        slider.value = 0;
     });
 
     textTwo.addEventListener('click', function() {
-        for(let i=0; i < arrowAll.length; i = i + 1) {
-            arrowAll[i].style.display = "none";
-        }
-        arrowTwo.style.display = 'block';
+        slider.value = 20;
     });
 
     textThree.addEventListener('click', function() {
-        for(let i=0; i < arrowAll.length; i = i + 1) {
-            arrowAll[i].style.display = "none";
-        }
-        arrowThree.style.display = 'block';
+        slider.value = 49.5;
     });
 
     textFour.addEventListener('click', function() {
-        for(let i=0; i < arrowAll.length; i = i + 1) {
-            arrowAll[i].style.display = "none";
-        }
-        arrowFour.style.display = 'block';
+        slider.value = 100;
     });
 });
