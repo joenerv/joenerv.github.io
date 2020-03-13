abf89171d3705043352f646b412681c939124cba
joenerv <joenerv@yandex.ru>
2020-03-13 16:03:17 +0300

    develop

---
+++ Шаблон для верстки/dist/js/script.js
@@ -0,0 +1,15 @@
+// шаблон для js(Обработчик готовности дерева DOM)
+
+window.addEventListener('DOMContentLoaded', function() {
+
+    'use strict';
+
+    
+});
+
+//шаблон для jQuery(Обработчик готовности дерева DOM)
+
+$(document).ready(function(){
+
+    
+  });
