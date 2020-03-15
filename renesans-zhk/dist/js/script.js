237636142256125187c86ed9b2ef23904faed9c8
joenerv <joenerv@yandex.ru>
2020-03-15 13:57:46 +0300

    up

--- renesans-zhk/dist/js/script.js
+++ renesans-zhk/dist/js/script.js
@@ -4,12 +4,32 @@ window.addEventListener('DOMContentLoaded', function() {
 
     'use strict';
 
-    
+    var mySwiper = new Swiper ('.swiper-container', {
+      // Optional parameters
+      direction: 'vertical',
+      loop: true,
+  
+      // If we need pagination
+      pagination: {
+        el: '.swiper-pagination',
+      },
+  
+      // Navigation arrows
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+  
+      // And if we need scrollbar
+      scrollbar: {
+        el: '.swiper-scrollbar',
+      },
+    })
 });
 
 //шаблон для jQuery(Обработчик готовности дерева DOM)
 
-$(document).ready(function(){
+// $(document).ready(function(){
 
     
-  });
+//   });
