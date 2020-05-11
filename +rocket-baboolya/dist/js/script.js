797b7b3f5aac6c773e3a8ee19536afc3cde332a0
joenerv <joenerv@yandex.ru>
2020-05-11 15:19:33 +0300

    r

---
+++ +rocket-baboolya/dist/js/script.js
@@ -0,0 +1,19 @@
+// шаблон для js(Обработчик готовности дерева DOM)
+
+window.addEventListener('DOMContentLoaded', function() {
+
+    'use strict';
+
+    
+});
+
+//шаблон для jQuery(Обработчик готовности дерева DOM)
+
+$(document).ready(function(){
+  
+  // $(function() {
+  //   $('select').selectric();
+  // });
+    
+    
+  });
