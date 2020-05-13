88cefe1267f467c9b72866678b53f507ec729f14
joenerv <joenerv@yandex.ru>
2020-05-13 14:20:37 +0300

    r

--- +rocket-baboolya/dist/js/script.js
+++ +rocket-baboolya/dist/js/script.js
@@ -4,7 +4,91 @@ window.addEventListener('DOMContentLoaded', function() {
 
     'use strict';
 
-    
+
+  let metroWrap = document.getElementById('metro-wrap');
+  let flatWrap = document.getElementById('flat-wrap');
+  let priceWrap = document.getElementById('price-wrap');
+  let popupMetro = document.querySelector('.popup-metro');
+  let popupFlat = document.querySelector('.popup-flat');
+  let popupPrice = document.querySelector('.popup-price');
+  let priceMin = document.querySelector('.selectbox-price__min');
+  let priceMax = document.querySelector('.selectbox-price__max');
+  let inputPriceMin = document.getElementById('input-price-min');
+  let inputPriceMax = document.getElementById('input-price-max');
+  let popupPriceMinReset = document.querySelector('.popup-price-min__reset');
+  let popupPriceMaxReset = document.querySelector('.popup-price-max__reset');
+
+
+
+  metroWrap.addEventListener('click', function() {
+
+    if (popupMetro.style.display = 'none') {
+      popupMetro.style.display = 'inline-block';
+      metroWrap.classList.add('selectbox-wrap__open');
+    } 
+
+  });
+
+  flatWrap.addEventListener('click', function() {
+    popupFlat.style.display = 'inline-block';
+    flatWrap.classList.add('selectbox-wrap__open');
+  });
+
+  priceWrap.addEventListener('click', function() {
+    popupPrice.style.display = 'inline-block';
+    priceWrap.classList.add('selectbox-wrap__open');
+  });
+
+  // ф-ия удаляет введеную стоимость в полях ввода
+  popupPriceMinReset.addEventListener('click', function() {
+    inputPriceMin.placeholder = "";
+    inputPriceMin.value = "";
+
+  });
+
+  popupPriceMaxReset.addEventListener('click', function() {
+    inputPriceMax.placeholder = "";
+    inputPriceMax.value = "";
+
+  });
+
+  // ф-ия добавляет введеное значение в поле
+  
+  inputPriceMin.addEventListener('change', () => {
+    priceMin.textContent = prettify(inputPriceMin.value);
+
+  });  
+
+
+  inputPriceMax.addEventListener('change', () => {
+    priceMax.textContent = prettify(inputPriceMax.value);
+
+  });
+
+  // ф-я разделения чисел
+  function prettify(num) {
+    var n = num.toString();
+    return n.replace(/(\d{1,3}(?=(?:\d\d\d)+(?!\d)))/g, "$1" + ' ');
+  }
+
+  // swiper
+  var mySwiper = new Swiper ('.mobile-slider', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: false,
+    slidesPerView: 2,
+    spaceBetween: 10,
+
+
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    }
+
+
+  })
+
 });
 
 //шаблон для jQuery(Обработчик готовности дерева DOM)
