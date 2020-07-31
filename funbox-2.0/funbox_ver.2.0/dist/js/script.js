5dd3caa86956430bed8f094ddb903511af6c3e48
joenerv <joenerv@yandex.ru>
2020-07-31 19:24:55 +0300

    up

--- funbox-2.0/funbox_ver.2.0/dist/js/script.js
+++ funbox-2.0/funbox_ver.2.0/dist/js/script.js
@@ -4,12 +4,54 @@ window.addEventListener('DOMContentLoaded', function() {
 
     'use strict';
 
+    let card = document.querySelectorAll('.card');
+    let pay = document.querySelectorAll('.card-footer__pay');
+
+    // выбор пачки при нажатии на пачку
+    card.forEach(elem => {
+
+      elem.addEventListener('click', () => {        
+        if (elem.classList.contains('selected')) {
+          elem.addEventListener('mouseleave', () => {            
+            elem.classList.remove('selected');
+          });
+
+        } else if (elem.classList.contains('disabled')) {
+          elem.addEventListener('mouseleave', () => {            
+            elem.classList.remove('selected');
+          });
+        } else {
+
+          elem.addEventListener('mouseleave', () => {
+            elem.classList.add('selected');
+          });
+        }
+        
+      });
+    })
+    
+    // выбор пачки  при нажатии на "купи"
+    pay.forEach(elem => {
+      elem.addEventListener('click', () => {
+        
+      })
+    })
+
+    // двойной клик (пачка недоступна)
+
+    card.forEach(elem => {
+
+      elem.addEventListener('dblclick', () => {
+        if (elem.classList.contains('disabled')) {
+          elem.classList.remove('disabled');
+        } else {
+          
+          elem.addEventListener('mouseleave', () => {            
+            elem.classList.remove('selected');
+          });
+          elem.classList.add('disabled');
+        }
+      });
+    })
     
 });
-
-//шаблон для jQuery(Обработчик готовности дерева DOM)
-
-$(document).ready(function(){
-
-    
-  });
