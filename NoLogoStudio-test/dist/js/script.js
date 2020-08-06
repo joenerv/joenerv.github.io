a1a4b6ad217431062451918d632ce21bc8a6f0da
joenerv <joenerv@yandex.ru>
2020-08-06 14:02:26 +0300

    NoLogoStudio

---
+++ NoLogoStudio-test/dist/js/script.js
@@ -0,0 +1,14 @@
+// шаблон для js(Обработчик готовности дерева DOM)
+
+window.addEventListener('DOMContentLoaded', function() {
+
+    'use strict';
+    
+});
+
+//шаблон для jQuery(Обработчик готовности дерева DOM)
+
+$(document).ready(function(){
+
+  $('ul.menu.flex').flexMenu();
+  });
