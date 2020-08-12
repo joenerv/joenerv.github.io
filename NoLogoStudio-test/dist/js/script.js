59b3150836bb1e899285b049f2e47bff8785ca57
joenerv <joenerv@yandex.ru>
2020-08-12 17:25:50 +0300

    up

--- NoLogoStudio-test/dist/js/script.js
+++ NoLogoStudio-test/dist/js/script.js
@@ -3,6 +3,67 @@
 window.addEventListener('DOMContentLoaded', function() {
 
     'use strict';
+
+    let menuItem = document.querySelectorAll('.menu-item');
+    let menuSubmenu = document.querySelector('.menu-submenu');
+    let submenuItem = document.querySelectorAll('.submenu-item');
+    let submenuFood = document.querySelector('.submenu-food');
+    let submenuGas = document.querySelector('.submenu-gas');
+    let submenuChemistry = document.querySelector('.submenu-chemistry');
+
+
+    // клик на меню второго порядка
+    let a = Array.from(submenuItem);
+
+    a.forEach(elem => {
+
+      elem.addEventListener('click', () => {
+        let b = a.indexOf(elem);
+        
+        if (b == 0) {
+          submenuFood.classList.toggle('show-submenu');
+          submenuGas.classList.remove('show-submenu');
+          submenuChemistry.classList.remove('show-submenu');
+        } else if (b == 1) {
+          submenuGas.classList.toggle('show-submenu');
+          submenuFood.classList.remove('show-submenu');
+          submenuChemistry.classList.remove('show-submenu');
+        } else if (b == 2) {
+          submenuChemistry.classList.toggle('show-submenu');
+          submenuFood.classList.remove('show-submenu');
+          submenuGas.classList.remove('show-submenu');
+        }
+      });
+    })
+
+    // клик на меню первого порядка
+
+    menuItem.forEach(elem => {
+
+      elem.addEventListener('click', () => {
+
+        submenuFood.classList.remove('show-submenu');
+        submenuGas.classList.remove('show-submenu');
+        submenuChemistry.classList.remove('show-submenu');
+
+
+
+        if (elem.classList.contains('menu-active')) {
+
+          elem.classList.remove('menu-active');
+          menuSubmenu.classList.remove('show');
+
+        } else {
+          
+          menuItem.forEach(elem => {
+            elem.classList.remove('menu-active');
+          })
+          elem.classList.add('menu-active');
+          menuSubmenu.classList.add('show');
+
+        }
+      })
+    })
     
 });
 
