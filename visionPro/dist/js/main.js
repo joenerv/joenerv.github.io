cf359942209aa0984b56e8e2cd9a102a1847d442
Evgenii <joenerv@yandex.ru>
2022-03-07 10:12:46 +0300

    ffff

--- visionPro/dist/js/main.js
+++ visionPro/dist/js/main.js
@@ -2,10 +2,12 @@ window.addEventListener('DOMContentLoaded', function() {
 
 	'use strict';
 
-	/*
+
 	let info = document.querySelector('.info');//header
 
 	window.addEventListener('scroll', () => {
+		console.log(window.innerHeight);
+		console.log(window.pageYOffset);
 		if(window.pageYOffset > window.innerHeight) {
 			info.classList.add('scroll');
 		} else {
@@ -13,7 +15,20 @@ window.addEventListener('DOMContentLoaded', function() {
 		}
 	});
 
-	*/
+
+	// для поворота глаза
+	let bannerLogoRing = document.querySelector('.js_banner_logo_ring');
+
+	document.querySelector('body').addEventListener('mousemove', (e) => {
+		let x = e.clientX * 100 / window.innerWidth + "%";
+		let y = e.clientY * 100 / window.innerHeight + "%";
+		console.log(x + ' ' + y);
+		bannerLogoRing.style.left = x ;
+		bannerLogoRing.style.top = y;
+		bannerLogoRing.style.transform = "translate(-" + x + ", -" + y + ")";
+
+	});
+
 
 	// скролл вниз при клике на кнопку
 	let bannerDown = document.querySelector('.js_banner_down');//
